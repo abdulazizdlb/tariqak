60ecc15f4155c830d3ae5498ef281757bc98e06d
abdulazizdlb <82365260+abdulazizdlb@users.noreply.github.com>
2025-09-07 21:27:00 +0300

    Update Commute.swift

--- Models/Commute.swift
+++ Models/Commute.swift
@@ -2,15 +2,22 @@ import Foundation
 
 struct Commute: Identifiable, Hashable {
     var id = UUID()
-
-    // عناوين يكتبها المستخدم – مستخدمة في PredictionEngine
+    
+    // عناوين يكتبها المستخدم
     var homeAddress: String = ""
     var workAddress: String = ""
-
+    
     // توافق خلفي: لو كنت تستخدم إحداثيات سابقًا خلّيناها اختيارية
     var home: Coordinate? = nil
     var work: Coordinate? = nil
-
-    // وقت الانطلاق (إن احتجته)
+    
+    // وقت الانطلاق
     var departure: Date = Date()
+    
+    // أيام الأسبوع (0=الأحد، 1=الاثنين، ... 6=السبت)
+    var selectedDays: Set<Int> = []
+    
+    // النافذة الزمنية
+    var windowStart: Date = Date()
+    var windowEnd: Date = Calendar.current.date(byAdding: .hour, value: 1, to: Date()) ?? Date()
 }
