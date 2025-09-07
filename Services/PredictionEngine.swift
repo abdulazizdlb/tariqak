228affbb0ab471743f322f09f5823d74c2741616
abdulazizdlb <82365260+abdulazizdlb@users.noreply.github.com>
2025-09-07 21:54:01 +0300

    Update PredictionEngine.swift

--- Services/PredictionEngine.swift
+++ Services/PredictionEngine.swift
@@ -8,27 +8,14 @@ struct Prediction {
 struct PredictionEngine {
     let provider: HereTrafficProvider
     
-    /// حساب أفضل وقت للمغادرة
     func bestDeparture(for commute: Commute) async throws -> Prediction {
-        // 1) Geocode addresses
         let homeCoords = try await provider.geocode(address: commute.homeAddress)
         let workCoords = try await provider.geocode(address: commute.workAddress)
-        
-        // 2) Calculate current route duration
         let currentDuration = try await provider.routeMinutes(from: homeCoords, to: workCoords)
         
-        // 3) Calculate optimal departure time (simple algorithm)
-        let bestTime = calculateOptimalTime(from: commute.windowStart, to: commute.windowEnd)
-        
         return Prediction(
-            bestTime: bestTime,
+            bestTime: Date().addingTimeInterval(-15 * 60), // 15 minutes from now
             expectedDurationMinutes: currentDuration
         )
-    }
-    
-    private func calculateOptimalTime(from start: Date, to end: Date) -> Date {
-        // Simple: suggest 15 minutes before midpoint
-        let midpoint = start.addingTimeInterval((end.timeIntervalSince(start)) / 2)
-        return midpoint.addingTimeInterval(-15 * 60)
     }
 }
